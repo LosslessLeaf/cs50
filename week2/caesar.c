7b2d8190d85c4d6104dc6c1701552195f77cc848
Joseph LoPresti <josephlopresti.student@careerdevs.com>
2019-05-11 14:14:46 +0000

    Caesar finished

--- week2/caesar.c
+++ week2/caesar.c
@@ -1,97 +1,76 @@
 #include <cs50.h>
+#include <ctype.h>
 #include <stdio.h>
 #include <string.h>
 
 int main(int argc, string argv[])
 {
-
-
-    // printf("%i\n", key);
     if (argc > 1)
     {
         string str = argv[1];
-        // int i;
-        // int strLength = strlen(str);
-        // int counter = 0;
-        // int x;
-        int key = atoi(argv[1]), strLength = strlen(str), counter = 0, x, i;
+        int key = atoi(argv[1]), counter = 0, x, i, letter;
         char y;
-        // printf("%i\n", strLength);
 
-        for (i = 0; i < strLength; i++)
+        for (i = 0; i < strlen(str); i++)
         {
-            // printf("%c\n", str[i]);
 
             x = str[i] - '0';
-            // printf("%i\n", x);
 
             if (x >= 0 && x <= 9)
             {
-                // printf("%i\n", x);
                 counter++;
             }
         }
-        // printf("%i, %s\n", counter, str);
         if (counter == strlen(str) && argc == 2)
         {
-            // printf("Good!\n");
+
             string plaintext = get_string("plaintext: ");
-            // printf("%s\n", plaintext);
-            int textLength = strlen(plaintext);
-            // printf("%i\n", textLength);
-
-            for (i = 0; i < textLength; i++)
+            printf("ciphertext: ");
+            for (i = 0; i < strlen(plaintext); i++)
             {
-                // printf("%c\n", plaintext[i]);
-                x = plaintext[i];
-                if (x >= 65 && x <= 90)
+                if (isalpha(plaintext[i]))
                 {
-                    // printf("Real %i\n", x);
-                    x = x + key;
-                    // printf("%i\n", x);
-                    if (x > 90)
-                    {
-                        x -= 26;
-                    }
-                    y = x;
-                    printf("%c", x);
-                }
-                else if (x >= 97 && x <= 122)
-                {
-                    // printf("real");
-                    // printf("%i\n", key);
-                    x = x + key;
-                    if (x > 122 && x < 148)
-                    {
-                        x -= 26;
-                    }
-                    else if (x >= 148)
-                    {
-                        int k = x % 26;
-                        x = 122 - k;
-                    }
-                    y = x;
-                    printf("%c", y);
+                    letter = plaintext[i];
 
+                    if (letter >= 65 && letter <= 90)
+                    {
+                        letter = plaintext[i] + (key % 26);
+                        if (letter > 90)
+                        {
+                            letter = 64 + (letter - 90);
+                            printf("%c", letter);
+                        }
+                        else
+                        {
+                            printf("%c", letter);
+                        }
+                    }
+                if (letter >= 97 && letter <= 122)
+                {
+                    letter = plaintext[i] + (key % 26);
+                    if (letter > 122)
+                    {
+                        letter = 96 + (letter - 122);
+
+                        printf("%c", letter);
+                    }
+                    else
+                    {
+                        printf("%c", letter);
+                    }
+                }
                 }
                 else
                 {
-                    // printf("fake");
-                    y = x;
-                    printf("%i", y);
+                    printf("%c", plaintext[i]);
                 }
-
-                // if (x >= 0 && x <= 9)
-                // {
-
-                // }
             }
-
+            printf("\n");
         }
         else if (counter != strlen(str) || argc > 2 || argc <= 0)
         {
             printf("Usage: ./caesar key\n");
-            return 0;
+            return 1;
         }
         else
         {
@@ -101,7 +80,7 @@ int main(int argc, string argv[])
     else
     {
         printf("Usage: ./caesar key\n");
-        return 0;
+        return 1;
     }
 
 }
